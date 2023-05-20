1d4a3ff8d6f6e036437c52a52626ee73d3a60172
Jim VA <37945754+Jim-VA@users.noreply.github.com>
2023-05-20 13:48:01 -0600

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('Desarrollo sitios web y escribo artículos sobre programación.')
+  .typeString('Desarrollo sitios web y artículos sobre tecnología y vida académica.')
   .pauseFor(200)
   .deleteChars(10)
   .start();
